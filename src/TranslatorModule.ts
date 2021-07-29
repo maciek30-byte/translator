87a10b169621156b4fdb01a84d655de7826252af
Maciek <maciek29.opozda@gmail.com>
2021-07-29 11:24:18 +0200

    - Detected language, and translate function is working

---
+++ src/TranslatorModule.ts
@@ -0,0 +1,32 @@
+const { Translate } = require("@google-cloud/translate").v2;
+require("dotenv").config();
+// @ts-ignore
+const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
+
+// cliient configuration
+const translate = new Translate({
+  credentials: CREDENTIALS,
+  projectId: CREDENTIALS.project_id,
+});
+
+class TranslatorModule {
+  async detectLanguage(text: string) {
+    try {
+      let response = await translate.detect(text);
+      return response[0].language;
+    } catch (error) {
+      console.log(`Error at detectLanguage --> ${error}`);
+      return 0;
+    }
+  }
+  async translateText(text: string, targetLanguage: string) {
+    try {
+      let [response] = await translate.translate(text, targetLanguage);
+      return response;
+    } catch (error) {
+      console.log(`Error at translateText --> ${error}`);
+      return 0;
+    }
+  }
+}
+export default TranslatorModule
